a457e1fd46d6b4740dd06ac00980032a168d56a1
Jin-Dong Kim <jindong.kim@gmail.com>
2014-11-14 17:23:33 +0900

    project specificatin enabled.

--- pubannotation-sparql.rb
+++ pubannotation-sparql.rb
@@ -13,13 +13,12 @@ class PubAnnotationSPARQL < Sinatra::Base
 		default_graph_uri = params["default-graph-uri"]
 		query						  = params["query"]
 
-		# p = default_graph_uri.rindex('/')
-		# @project = default_graph_uri[p + 1 .. -1]
-
     endpoint = SPARQL::Client.new("http://rdf.pubannotation.org/sparql")
 		@results = if default_graph_uri.nil? || default_graph_uri.empty?
 			endpoint.query(query)
 		else
+			p = default_graph_uri.rindex('/')
+			@project = default_graph_uri[p + 1 .. -1]
 			endpoint.query(query, "default-graph-uri" => default_graph_uri)
 		end
 
