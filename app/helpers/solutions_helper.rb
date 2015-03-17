a21504293b73fa7b83d248034902a2f896db377d
Jin-Dong Kim <jindong.kim@gmail.com>
2015-03-17 17:25:58 +0900

    variable independency

--- app/helpers/solutions_helper.rb
+++ app/helpers/solutions_helper.rb
@@ -5,29 +5,32 @@ module SolutionsHelper
 		projects = [projects] unless projects.respond_to?(:each)
 		span_annotations_urls = []
 		solutions.each_solution do |solution|
-		  vars = solution.to_h.keys
-		  s = solution[vars[0]].to_s
-		  p = s.rindex('/')
-		  url = s[0 .. p]
-		  span = s[p + 1 .. -1]
-		  mbeg, mend = span.split('-')
-		  (1 ... vars.length).each do |i|
-		    s = solution[vars[i]].to_s
-		    p = s.rindex('/')
-		    span = s[p + 1 .. -1]
-		    sbeg, send = span.split('-')
-		    mbeg = sbeg if sbeg < mbeg
-		    mend = send if send > mend
-		  end
-		  span_url = url + mbeg + '-' + mend
-		  span_annotations_url = span_url + '/annotations.json'
-		  options = []
-		  options << 'context_size=' + context_size if context_size > 0
-		  options << 'project=[' + projects.join(',') + ']' if projects.present?
-		  span_annotations_url += '?' + options.join('&') if options.present?
-		  span_annotations_urls << [span_url, span_annotations_url]
+			span_urls = solution.to_h.values.select{|v| span?(v)}.map{|v| v.to_s}
+			unless span_urls.empty?
+				ranges = span_urls.map{|u| span_begin_end(u)}
+				mbeg = ranges.map{|r| r[0]}.min
+				mend = ranges.map{|r| r[1]}.max
+				span_url = "#{span_prefix(span_urls[0])}#{mbeg}-#{mend}"
+				span_annotations_url = span_url + '/annotations.json'
+			  options = []
+			  options << 'context_size=' + context_size if context_size > 0
+			  options << 'project=[' + projects.join(',') + ']' if projects.present?
+			  span_annotations_url += '?' + options.join('&') if options.present?
+			  span_annotations_urls << [span_url, span_annotations_url]
+			end
 		end
 		span_annotations_urls
 	end
 
+	def span?(v)
+		!!(%r|/spans/\d+-\d+$|.match(v))
+	end
+
+	def span_prefix(span_url)
+		span_url[0..span_url.rindex('/')]
+	end
+
+	def span_begin_end(span_url)
+		span_url[span_url.rindex('/') + 1 .. -1].split('-').map{|p| p.to_i}
+	end
 end
