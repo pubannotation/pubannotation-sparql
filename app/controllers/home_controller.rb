70ce0dc97609b4509e38e9f29198398f60ecef00
Jin-Dong Kim <jindong.kim@gmail.com>
2015-03-19 14:05:26 +0900

    default query is read from the first example.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,13 @@ class HomeController < ApplicationController
 		@default_graph_uri = params["default-graph-uri"]
 		@query						 = params["query"]
 
+		unless @query.present?
+			default_query = Query.find_by_priority(1)
+			@default_graph_uri = default_query.graph
+			@query             = default_query.sparql
+			@comment           = default_query.comment
+		end
+
 		@queries = Query.order(:priority).all
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -15,4 +15,18 @@ HERE
 
 		end
 	end
+
+	def default_comment
+		if @comment.present?
+			@comment
+		else
+
+<<HERE
+<h1>Tip</h1>
+Clicking on an example query will show the SPARQL query and an explanation.
+HERE
+
+		end
+	end
+
 end
